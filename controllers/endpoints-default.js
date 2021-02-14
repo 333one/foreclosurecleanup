f6516a66503db06905d8d866977277691642f33a
Al Rosetti <thr333one@gmail.com>
2021-02-13 20:08:37 -0700

    News section started

--- controllers/endpoints-default.js
+++ controllers/endpoints-default.js
@@ -9,6 +9,7 @@ const urlEncoded = bodyParser.urlencoded({ extended: true });
 
 router.get(['/', '/index'], middlewareDefault.homePage);
 router.get('/foreclosure-cleanup-vendor-list', middlewareDefault.foreclosureCleanupVendorList);
+router.get('/news', middlewareDefault.news);
 router.get('/privacy-policy', middlewareDefault.privacyPolicy);
 router.get('/terms-of-service', middlewareDefault.termsOfService);
 
--- controllers/middleware-default.js
+++ controllers/middleware-default.js
@@ -30,55 +30,22 @@ exports.foreclosureCleanupVendorList = wrapAsync(async function (req, res) {
 
 	let { searchType } = req.query;
 	if (searchType === 'new' || searchType === 'multiple') {
-		let cleanedQuery = submissionProcessing.cleanQuery(
-			formFields.foreclosureCleanupVendorList,
-			req.query
-		);
-		let cleanedQueryWithBoolean = logicDefault.convertStringToBoolean(
-			cleanedQuery,
-			defaultValue.listOfCompanyServices
-		);
+		let cleanedQuery = submissionProcessing.cleanQuery(formFields.foreclosureCleanupVendorList, req.query);
+		let cleanedQueryWithBoolean = logicDefault.convertStringToBoolean(cleanedQuery, defaultValue.listOfCompanyServices);
 
-		let {
-			searchRadius,
-			searchZipCode,
-			boardingSecuring,
-			debrisRemovalTrashout,
-			evictionManagement,
-			fieldInspection,
-			handymanGeneralMaintenance,
-			landscapeMaintenance,
-			lockChanges,
-			overseePropertyRehabilitation,
-			poolMaintenance,
-			propertyCleaning,
-			winterizations
-		} = cleanedQueryWithBoolean;
+		let { searchRadius, searchZipCode, boardingSecuring, debrisRemovalTrashout, evictionManagement, fieldInspection, handymanGeneralMaintenance, landscapeMaintenance, lockChanges, overseePropertyRehabilitation, poolMaintenance, propertyCleaning, winterizations } = cleanedQueryWithBoolean;
 
 		searchRadiusNumerical = parseInt(searchRadius);
-		searchRadiusAttributesAndValue = logicDefault.createSearchRadiusAttributesAndValue(
-			defaultValue.searchRadius,
-			searchRadiusNumerical
-		);
-		servicesAttributesAndValues = logicDefault.createServicesAttributesAndValues(
-			defaultValue.listOfCompanyServices,
-			cleanedQueryWithBoolean
-		);
+		searchRadiusAttributesAndValue = logicDefault.createSearchRadiusAttributesAndValue(defaultValue.searchRadius, searchRadiusNumerical);
+		servicesAttributesAndValues = logicDefault.createServicesAttributesAndValues(defaultValue.listOfCompanyServices, cleanedQueryWithBoolean);
 
 		let isSearchZipCodeFilled = searchZipCode === '' ? false : true;
 		let regSearchZipCode = new RegExp(regExpValue.companyZip);
-		let isSearchZipCodeValidCharacters = regSearchZipCode.test(
-			searchZipCode
-		);
-		let isSearchZipCodeFiveDigits =
-			searchZipCode.length === 5 ? true : false;
+		let isSearchZipCodeValidCharacters = regSearchZipCode.test(searchZipCode);
+		let isSearchZipCodeFiveDigits = searchZipCode.length === 5 ? true : false;
 
 		// Don't search for zip code lat and long if there is an error.
-		if (
-			isSearchZipCodeFilled === true &&
-			isSearchZipCodeValidCharacters === true &&
-			isSearchZipCodeFiveDigits === true
-		) {
+		if (isSearchZipCodeFilled === true && isSearchZipCodeValidCharacters === true && isSearchZipCodeFiveDigits === true) {
 			let geoLocationOfZip = await client.geocodeForward(searchZipCode, {
 				autocomplete: false,
 				limit: 3,
@@ -86,32 +53,17 @@ exports.foreclosureCleanupVendorList = wrapAsync(async function (req, res) {
 			});
 
 			if (geoLocationOfZip.entity.features.length > 0) {
-				let geoLocationAnswerLatLong = logicDefault.getGeoLocationAnswerLatLong(
-					searchZipCode,
-					geoLocationOfZip
-				);
-				var {
-					isZipCodeRealAndInUsa,
-					zipCodeLongAndLat
-				} = geoLocationAnswerLatLong;
+				let geoLocationAnswerLatLong = logicDefault.getGeoLocationAnswerLatLong(searchZipCode, geoLocationOfZip);
+				var { isZipCodeRealAndInUsa, zipCodeLongAndLat } = geoLocationAnswerLatLong;
 			}
 		}
 
 		let isSearchRadiusFilled = searchRadius === '' ? false : true;
-		let isSearchRadiusValid = defaultValue.searchRadius.includes(
-			searchRadiusNumerical
-		);
+		let isSearchRadiusValid = defaultValue.searchRadius.includes(searchRadiusNumerical);
 
-		let searchServicesForChecking = logicDefault.createSearchServicesForChecking(
-			cleanedQueryWithBoolean,
-			defaultValue.listOfCompanyServices
-		);
-		let isAtLeastOneServiceChecked = checks.checkIfAtLeastOneCompanyServiceFilled(
-			searchServicesForChecking
-		);
-		let wereAllServiceValuesValid = checks.checkIfServiceValuesValid(
-			searchServicesForChecking
-		);
+		let searchServicesForChecking = logicDefault.createSearchServicesForChecking(cleanedQueryWithBoolean, defaultValue.listOfCompanyServices);
+		let isAtLeastOneServiceChecked = checks.checkIfAtLeastOneCompanyServiceFilled(searchServicesForChecking);
+		let wereAllServiceValuesValid = checks.checkIfServiceValuesValid(searchServicesForChecking);
 
 		let privacyTerms;
 		if (req.session.transfer) {
@@ -120,73 +72,41 @@ exports.foreclosureCleanupVendorList = wrapAsync(async function (req, res) {
 		} else {
 			privacyTerms = 'notChecked';
 		}
-		let privacyTermsBoolean = submissionProcessing.convertCheckboxToBoolean(
-			privacyTerms
-		);
+		let privacyTermsBoolean = submissionProcessing.convertCheckboxToBoolean(privacyTerms);
 		let isPrivacyTermsChecked = privacyTermsBoolean === true ? true : false;
 
 		// Don't search the DB if there are any errors.
-		if (
-			isSearchZipCodeFilled === true &&
-			isSearchZipCodeValidCharacters === true &&
-			isZipCodeRealAndInUsa === true &&
-			isSearchRadiusFilled === true &&
-			isSearchRadiusValid === true &&
-			isAtLeastOneServiceChecked === true &&
-			wereAllServiceValuesValid === true &&
-			isPrivacyTermsChecked === true
-		) {
-			let mongooseSearchObject = mongooseLogic.createMongooseSearchObject(
-				searchRadiusNumerical,
-				zipCodeLongAndLat,
-				{
-					boardingSecuring,
-					debrisRemovalTrashout,
-					evictionManagement,
-					fieldInspection,
-					handymanGeneralMaintenance,
-					landscapeMaintenance,
-					lockChanges,
-					overseePropertyRehabilitation,
-					poolMaintenance,
-					propertyCleaning,
-					winterizations
-				}
-			);
+		if (isSearchZipCodeFilled === true && isSearchZipCodeValidCharacters === true && isZipCodeRealAndInUsa === true && isSearchRadiusFilled === true && isSearchRadiusValid === true && isAtLeastOneServiceChecked === true && wereAllServiceValuesValid === true && isPrivacyTermsChecked === true) {
+			let mongooseSearchObject = mongooseLogic.createMongooseSearchObject(searchRadiusNumerical, zipCodeLongAndLat, {
+				boardingSecuring,
+				debrisRemovalTrashout,
+				evictionManagement,
+				fieldInspection,
+				handymanGeneralMaintenance,
+				landscapeMaintenance,
+				lockChanges,
+				overseePropertyRehabilitation,
+				poolMaintenance,
+				propertyCleaning,
+				winterizations
+			});
 
 			let mongooseSelectObject = mongooseLogic.createMongooseSelectObject();
 
-			let searchResults = await User.find(mongooseSearchObject)
-				.select(mongooseSelectObject)
-				.lean();
+			let searchResults = await User.find(mongooseSearchObject).select(mongooseSelectObject).lean();
 
-			let freeAccounts = logicDefault.separateAccountTypes(
-				searchResults,
-				defaultValue.accountDefault
-			);
-			let premiumAccounts = logicDefault.separateAccountTypes(
-				searchResults,
-				defaultValue.accountUpgrade
-			);
+			let freeAccounts = logicDefault.separateAccountTypes(searchResults, defaultValue.accountDefault);
+			let premiumAccounts = logicDefault.separateAccountTypes(searchResults, defaultValue.accountUpgrade);
 
-			let freeAccountsProcessed = logicDefault.processCompanyForViewing(
-				freeAccounts
-			);
-			let premiumAccountsProcessed = logicDefault.processCompanyForViewing(
-				premiumAccounts,
-				defaultValue.accountUpgrade
-			);
+			let freeAccountsProcessed = logicDefault.processCompanyForViewing(freeAccounts);
+			let premiumAccountsProcessed = logicDefault.processCompanyForViewing(premiumAccounts, defaultValue.accountUpgrade);
 
-			let searchAgainNamesValues = logicDefault.getSearchAgainNamesValues(
-				cleanedQueryWithBoolean
-			);
+			let searchAgainNamesValues = logicDefault.getSearchAgainNamesValues(cleanedQueryWithBoolean);
 
 			// This tells EJS to render the search result or an empty result if nothing was found.
 			let searchOutput = {};
-			searchOutput.free =
-				Array.isArray(freeAccountsProcessed) === true ? true : false;
-			searchOutput.premium =
-				Array.isArray(premiumAccountsProcessed) === true ? true : false;
+			searchOutput.free = Array.isArray(freeAccountsProcessed) === true ? true : false;
+			searchOutput.premium = Array.isArray(premiumAccountsProcessed) === true ? true : false;
 
 			let companyLogoPath = defaultValue.vendorLogoViewFolder;
 
@@ -207,26 +127,13 @@ exports.foreclosureCleanupVendorList = wrapAsync(async function (req, res) {
 				searchAgainNamesValues
 			});
 		} else {
-			let searchZipCodeError = errorMessage.getSearchZipCodeError(
-				isSearchZipCodeFilled,
-				isSearchZipCodeValidCharacters,
-				isSearchZipCodeFiveDigits,
-				isZipCodeRealAndInUsa
-			);
-			let searchRadiusError = errorMessage.getSearchRadiusError(
-				isSearchRadiusFilled,
-				isSearchRadiusValid
-			);
-			let searchServicesError = errorMessage.getSearchServicesError(
-				isAtLeastOneServiceChecked,
-				wereAllServiceValuesValid
-			);
+			let searchZipCodeError = errorMessage.getSearchZipCodeError(isSearchZipCodeFilled, isSearchZipCodeValidCharacters, isSearchZipCodeFiveDigits, isZipCodeRealAndInUsa);
+			let searchRadiusError = errorMessage.getSearchRadiusError(isSearchRadiusFilled, isSearchRadiusValid);
+			let searchServicesError = errorMessage.getSearchServicesError(isAtLeastOneServiceChecked, wereAllServiceValuesValid);
 
 			// Only grab this error on new searches, not a "search again".
 			if (searchType !== 'multiple') {
-				var privacyTermsError = errorMessage.getPrivacyTermsError(
-					isPrivacyTermsChecked
-				);
+				var privacyTermsError = errorMessage.getPrivacyTermsError(isPrivacyTermsChecked);
 			}
 
 			return res.render('foreclosure-cleanup-vendor-list', {
@@ -246,12 +153,8 @@ exports.foreclosureCleanupVendorList = wrapAsync(async function (req, res) {
 		}
 	} else {
 		// For rendering.
-		searchRadiusAttributesAndValue = logicDefault.createSearchRadiusAttributesAndValue(
-			defaultValue.searchRadius
-		);
-		servicesAttributesAndValues = logicDefault.createServicesAttributesAndValues(
-			defaultValue.listOfCompanyServices
-		);
+		searchRadiusAttributesAndValue = logicDefault.createSearchRadiusAttributesAndValue(defaultValue.searchRadius);
+		servicesAttributesAndValues = logicDefault.createServicesAttributesAndValues(defaultValue.listOfCompanyServices);
 
 		res.render('foreclosure-cleanup-vendor-list', {
 			activeLink,
@@ -267,8 +170,6 @@ exports.foreclosureCleanupVendorList = wrapAsync(async function (req, res) {
 });
 
 exports.homePage = wrapAsync(async function (req, res) {
-	console.dir('show me');
-
 	// For rendering.
 	let activeLink = 'index';
 	let contactEmail = siteValue.contactEmail.email;
@@ -276,6 +177,16 @@ exports.homePage = wrapAsync(async function (req, res) {
 	let { projectStatus } = siteValue;
 
 	res.render('index', { activeLink, contactEmail, loggedIn, projectStatus });
+});
+
+exports.news = wrapAsync(async function (req, res) {
+	// For rendering.
+	let activeLink = 'news';
+	let contactEmail = siteValue.contactEmail.email;
+	let loggedIn = req.session.userValues ? true : false;
+	let { projectStatus } = siteValue;
+
+	res.render('news', { activeLink, contactEmail, loggedIn, projectStatus });
 });
 
 exports.pageNotFound = wrapAsync(async function (req, res) {
@@ -295,30 +206,12 @@ exports.pageNotFound = wrapAsync(async function (req, res) {
 
 exports.postForeclosureCleanupVendorList = wrapAsync(async function (req, res) {
 	// A checkbox has to be selected to return a value.  This sets the value to false or empty for all unreturned properties.
-	let {
-		searchRadius = '',
-		searchZipCode = '',
-		boardingSecuring = 'false',
-		debrisRemovalTrashout = 'false',
-		evictionManagement = 'false',
-		fieldInspection = 'false',
-		handymanGeneralMaintenance = 'false',
-		landscapeMaintenance = 'false',
-		lockChanges = 'false',
-		overseePropertyRehabilitation = 'false',
-		poolMaintenance = 'false',
-		propertyCleaning = 'false',
-		winterizations = 'false',
-		privacyTerms = 'notChecked',
-		searchType = ''
-	} = req.body;
+	let { searchRadius = '', searchZipCode = '', boardingSecuring = 'false', debrisRemovalTrashout = 'false', evictionManagement = 'false', fieldInspection = 'false', handymanGeneralMaintenance = 'false', landscapeMaintenance = 'false', lockChanges = 'false', overseePropertyRehabilitation = 'false', poolMaintenance = 'false', propertyCleaning = 'false', winterizations = 'false', privacyTerms = 'notChecked', searchType = '' } = req.body;
 
 	req.session.transfer = {};
 	req.session.transfer.privacyTerms = privacyTerms;
 
-	return res.redirect(
-		`/foreclosure-cleanup-vendor-list?searchRadius=${searchRadius}&searchZipCode=${searchZipCode}&boardingSecuring=${boardingSecuring}&debrisRemovalTrashout=${debrisRemovalTrashout}&evictionManagement=${evictionManagement}&fieldInspection=${fieldInspection}&handymanGeneralMaintenance=${handymanGeneralMaintenance}&landscapeMaintenance=${landscapeMaintenance}&lockChanges=${lockChanges}&overseePropertyRehabilitation=${overseePropertyRehabilitation}&poolMaintenance=${poolMaintenance}&propertyCleaning=${propertyCleaning}&winterizations=${winterizations}&searchType=${searchType}`
-	);
+	return res.redirect(`/foreclosure-cleanup-vendor-list?searchRadius=${searchRadius}&searchZipCode=${searchZipCode}&boardingSecuring=${boardingSecuring}&debrisRemovalTrashout=${debrisRemovalTrashout}&evictionManagement=${evictionManagement}&fieldInspection=${fieldInspection}&handymanGeneralMaintenance=${handymanGeneralMaintenance}&landscapeMaintenance=${landscapeMaintenance}&lockChanges=${lockChanges}&overseePropertyRehabilitation=${overseePropertyRehabilitation}&poolMaintenance=${poolMaintenance}&propertyCleaning=${propertyCleaning}&winterizations=${winterizations}&searchType=${searchType}`);
 });
 
 exports.privacyPolicy = wrapAsync(async function (req, res) {
